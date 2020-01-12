a8e753a283a3cbfac99b98c29257415d9d9a8425
lybimoff <lyubimoff@gmail.com>
2020-01-12 21:59:19 +0300

    update

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-import csv
+import pandas as pd
+from transliterate import translit
 
-with open('users.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
+newFile = r'D:\Dev\csvTranslit\newUsers.csv'
+file = r'D:\Dev\csvTranslit\users.csv'
+headers = ['username', 'password', 'lastname', 'firstname', 'middlename', 'email']
+myCSV = pd.read_csv(file, sep=';')
+df = pd.DataFrame(index=range(0, len(myCSV)), columns=headers)
+
+
+def translit_row(row):
+    username = translit(row[0], "ru", reversed=True).lower()
+    return pd.Series([username.replace("'", ""), row[1].replace(" ", ""), row[2].replace(" ", ""),
+                      row[3].replace(" ", ""), row[4].replace(" ", ""), row[5].replace(" ", "")], headers)
+
+
+for i, row in enumerate(myCSV.values):
+    df.loc[i] = translit_row(row)
+
+df.to_csv(newFile, sep=";", header=True, index=False)
+print(df)
