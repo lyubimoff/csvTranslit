7535761bfd476dfba2ffaa03e998420bcf05c95a
lybimoff <lyubimoff@gmail.com>
2020-03-25 13:15:20 +0300

    update

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import pandas as pd
 from transliterate import translit
+import string
+import random
+from sys import argv
 
-newFile = r'D:\Dev\csvTranslit\newUsers.csv'
-file = r'D:\Dev\csvTranslit\users.csv'
-headers = ['username', 'password', 'lastname', 'firstname', 'middlename', 'email']
+file, email = argv
+
+def randompassword():
+  chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
+  size = random.randint(8, 12)
+  return ''.join(random.choice(chars) for x in range(size))
+
+newFile = r'D:\Dev\PythonProjects\csvTranslit\new_username.csv'
+file = r'D:\Dev\PythonProjects\csvTranslit\username.csv'
+headers = ['username', 'password', 'lastname', 'firstname', 'middlename', 'email', 'cohort1']
 myCSV = pd.read_csv(file, sep=';')
 df = pd.DataFrame(index=range(0, len(myCSV)), columns=headers)
 
-
 def translit_row(row):
     username = translit(row[0], "ru", reversed=True).lower()
-    return pd.Series([username.replace("'", ""), row[1].replace(" ", ""), row[2].replace(" ", ""),
-                      row[3].replace(" ", ""), row[4].replace(" ", ""), row[5].replace(" ", "")], headers)
+    email = username.replace("'", "")+"@example.com"
+    return pd.Series([username.replace("'", ""), randompassword(), row[2].replace(" ", ""),
+                      row[3].replace(" ", ""), row[4].replace(" ", ""), email, row[6].replace(" ", "")], headers)
 
 
 for i, row in enumerate(myCSV.values):
     df.loc[i] = translit_row(row)
 
 df.to_csv(newFile, sep=";", header=True, index=False)
-print(df)
+print(df.all)
